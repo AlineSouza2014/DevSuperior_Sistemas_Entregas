7ad349bd21a49d1775208869e6e889245a0579e6
Aline Souza <aline-mac@hotmail.com>
2021-01-07 20:30:15 -0300

    Project created

---
+++ src/main/java/com/projetodevsuperior/sistema_de_entrega/SistemaDeEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.projetodevsuperior.sistema_de_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEntregaApplication.class, args);
+	}
+
+}
